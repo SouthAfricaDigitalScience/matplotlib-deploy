7934ef49fd97c7883c5104c49df20bc134f90e36
Sakhile Masoka <sakhile.harvey@gmail.com>
2015-10-13 15:46:55 +0200

    First build and check-build - Its should fail, im just gathering module information

---
+++ build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+#SOURCE_FILE=$NAME-$VERSION.tar.gz
+. /usr/share/modules/init/bash 
+# We will build the code from the github repo, but if we want specific versions,
+# a new Jenkins job will be created for the version number and we'll provide
+# the URL to the tarball in the configuration.
+SOURCE_REPO="https://downloads.sourceforge.net/project/matplotlib/matplotlib/matplotlib-1.4.3/"
+# We pretend that the $SOURCE_FILE is there, even though it's actually a dir.
+NAME="matplotlib"
+VERSION="1.4.3"
+SOURCE_FILE="$NAME-$VERSION.tar.gz"
+
+module load ci
+module load python/2.7.9
+module load numpy
+module load libpng/1.6.18-gcc-4.8.2
+module load freetype
+
+
+echo "REPO_DIR is "
+echo $REPO_DIR
+echo "SRC_DIR is "
+echo $SRC_DIR
+echo "WORKSPACE is "
+echo $WORKSPACE
+echo "SOFT_DIR is"
+echo $SOFT_DIR
+
+mkdir -p $WORKSPACE
+mkdir -p $SRC_DIR
+mkdir -p $SOFT_DIR
+
+#  Download the source file
+
+if [[ ! -e $SRC_DIR/$SOURCE_FILE ]] ; then
+  echo "seems like this is the first build - Let's get the $SOURCE_FILE from $SOURCE_REPO and unarchive to $WORKSPACE"
+  mkdir -p $SRC_DIR
+  wget $SOURCE_REPO/$SOURCE_FILE -O $SRC_DIR/$SOURCE_FILE
+else
+  echo "continuing from previous builds, using source at " $SRC_DIR/$SOURCE_FILE
+fi
+
+tar -xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
+cd $WORKSPACE/$NAME-$VERSION
+
+echo "Configuring the build"
+python setup.py build
---
+++ check-build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+. /usr/share/modules/init/bash
+module load ci
+module load python/2.7.9
+module load numpy
+module load libpng/1.6.18-gcc-4.8.2
+module load freetype
+
+echo "Running python check"
+cd $WORKSPACE/$NAME-$VERSION
+python setup.py check
+
+echo $?
+if [ $? != 0 ] ; then
+  exit 1
+fi
+
+
+echo "Running python install"
+python setup.py install
+
+mkdir -p modules
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+   puts stderr "\tAdds $NAME $VERSION to your environment"
+}
+
+module load gcc/$GCC_VERSION
+
+module-whatis   "$NAME $VERSION."
+setenv       MATPLOTLIB_VERSION       $VERSION
+setenv       MATPLOTLIB_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+
+prepend-path 	PATH            $::env(MATPLOTLIB_DIR)/bin
+prepend-path    PATH            $::env(MATPLOTLIB_DIR)/include
+prepend-path    PATH            $::env(MATPLOTLIB_DIR)/bin
+prepend-path    MANPATH         $::env(MATPLOTLIB_DIR)/man
+prepend-path    LD_LIBRARY_PATH $::env(MATPLOTLIB_DIR)/lib
+MODULE_FILE
+) > modules/$VERSION
+mkdir -p $LIBRARIES_MODULES/$NAME
+cp modules/$VERSION $LIBRARIES_MODULES/$NAME/$VERSION
+
+# Testing module
+module avail
+module list
+
